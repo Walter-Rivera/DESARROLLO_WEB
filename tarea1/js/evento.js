49dc03514f042dac0d1e44a17ef75ec2b0d2eaff
Walter-Rivera <giovannirivera1994.1@gmail.com>
2020-08-13 19:18:36 -0600

    agregando archivo js para ejecutar funcion que permita cambiar la ubicacion de pagina del frame

---
+++ tarea1/js/evento.js
@@ -0,0 +1,13 @@
+'use strict'
+
+    function enlazar(semestre)
+    {
+        if(semestre==1)
+        {
+            parent.inferior.location.href="../tarea1/frames/semestre1.html";
+        }
+        else if(semestre==2)
+        {
+            parent.inferior.location.href="../tarea1/frames/semestre2.html";
+        }
+    };
